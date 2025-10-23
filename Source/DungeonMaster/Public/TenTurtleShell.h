c2a9f6d8986100df7a03b837f5921b17b6a5c666
Derden <khkc9597@gmail.com>
2025-10-23 18:45:47 +0900

    feat(monsters): add 2 monster types with walk/idle animations
    - add Enemy(ChestMonster, TurtleShell)

---
+++ Source/DungeonMaster/Public/TenTurtleShell.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TenEnemyCharacter.h"
+#include "TenTurtleShell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNGEONMASTER_API ATenTurtleShell : public ATenEnemyCharacter
+{
+	GENERATED_BODY()
+	
+};
